e5b28f7ebee44c1f5100c7bf2449907424a4d9a8
Adrimi <adriano100417@gmail.com>
2020-05-25 23:41:08 +0200

    Initial Commit

---
+++ ResizableTextCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResizableTextCell
+//
+//  Created by adrian.szymanowski on 25/05/2020.
+//  Copyright © 2020 adrian.szymanowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
